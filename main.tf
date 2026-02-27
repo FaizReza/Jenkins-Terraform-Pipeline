e21dbc4f1f045588dd2e071726b4b67be7aa6fee
Faiz Sultan <85805423+FaizReza@users.noreply.github.com>
2026-02-27 11:41:56 +0530

    Refactor AWS provider and data block structure

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
 data "aws_ami" "myami" {
-
 most_recent      = true
-
 owners           = ["amazon"]
-
  filter {
     name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
-
-
 }
 
-
 resource "aws_instance" "public_instance" {
- ami           = "ami-0c398cb65a93047f2"
- instance_type = "t3.micro"
-
- tags = {
-   Name = var.name_tag,
- }
+  ami           = "ami-0c398cb65a93047f2"
+  instance_type = "t3.micro"
+  tags = {
+    Name = var.name_tag,
+  }
 }
